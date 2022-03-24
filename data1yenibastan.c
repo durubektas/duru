1aecefbd2fd161f5aaebbbda0bce467914801faa
durubektas <97095312+durubektas@users.noreply.github.com>
2022-03-25 01:43:32 +0300

    Add files via upload

--- data1yenibastan.c
+++ data1yenibastan.c
@@ -3,6 +3,26 @@
 
 void printList();
 void readfile();
+void calculate_row();
+void calculate_column();
+void row_show();
+void column_show();
+
+int row0 = 0;
+int row1 = 0;
+int row2 = 0;
+int row3 = 0;
+int row4 = 0;
+
+int column0 = 0;
+int column1 = 0;
+int column2 = 0;
+int column3 = 0;
+int column4 = 0;
+int column5 = 0;
+int column6 = 0;
+int column7 = 0;
+
 
 struct Node {
     int row;
@@ -17,7 +37,10 @@ struct Node *start=NULL;
 int main() {
 
     readfile();
-    printList();
+    calculate_row();
+    row_show();
+    calculate_column();
+    column_show();
 
 }
 
@@ -29,6 +52,39 @@ void printList() {
         printf("%d\t", cur->row);
         printf("%d\t", cur->column);
         printf("%d\n", cur->value);
+        cur=cur->next;
+    }
+}
+
+void calculate_row() {
+    struct Node *cur = start;
+    if ( cur == NULL) { printf("empty");}
+
+    while (cur != NULL ) {
+        if (cur->row == 0) { row0+=cur->value; }
+        if (cur->row == 1) { row1+=cur->value; }
+        if (cur->row == 2) { row2+=cur->value; }
+        if (cur->row == 3) { row3+=cur->value; }
+        if (cur->row == 4) { row4+=cur->value; }
+
+        cur=cur->next;
+    }
+}
+
+void calculate_column() {
+    struct Node *cur = start;
+    if ( cur == NULL) { printf("empty");}
+
+    while (cur != NULL ) {
+        if (cur->column == 0) { column0+=cur->value; }
+        if (cur->column == 1) { column1+=cur->value; }
+        if (cur->column == 2) { column2+=cur->value; }
+        if (cur->column == 3) { column3+=cur->value; }
+        if (cur->column == 4) { column4+=cur->value; }
+        if (cur->column == 5) { column5+=cur->value; }
+        if (cur->column == 6) { column6+=cur->value; }
+        if (cur->column == 7) { column7+=cur->value; }
+
         cur=cur->next;
     }
 }
@@ -80,4 +136,13 @@ void readfile() {
     }
 
     fclose(file);
+}
+
+void row_show(){
+    printf( "%d , %d, %d, %d, %d", row0, row1, row2, row3, row4);
+}
+
+void column_show(){
+    printf( "%d , %d, %d, %d, %d, %d, %d, %d", column0, column1, column2, column3, column4, column5,
+           column6, column7);
 }
