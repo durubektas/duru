e0e685f7236ef22cb562a27a06192f1556c55a0c
durubektas <97095312+durubektas@users.noreply.github.com>
2022-04-15 23:56:17 +0300

    Add files via upload

--- numeric1.c
+++ numeric1.c
@@ -10,11 +10,11 @@ int main() {
     double x_u= 0.5;
 
     bisection_method( x_l, x_u);
+    false_position( x_l, x_u) ;
 
 }
 
 void bisection_method( double x_l, double x_u) {
-
     if ( function(x_l)*function(x_u) > 0 ) { printf( "No root in this interval!" ); }
 
     printf( "Iteration no:\t x_l\t         x_u\t         x_r\t         f(x_r)\t         E_a\n");
@@ -46,4 +46,32 @@ double function(double t) {
 
 void false_position( double x_l, double x_u ) {
     if ( function(x_l)*function(x_u) > 0 ) { printf( "No root in this interval!" ); }
+
+    printf( "Iteration no:\t x_l\t         x_u\t         x_r\t         f(x_r)\t         E_a\n");
+    int no = 1;
+
+    double x_r = x_u - (function(x_u) * (x_u - x_l))/(function(x_u) - function(x_l)) ;
+
+    printf( "\t %d\t %f\t %f\t %f\t %f\t \n", no, x_l, x_u, x_r, function(x_r));
+    no++;
+
+    if ( function(x_r)*function(x_l) < 0) { x_u = x_r; }
+    else { x_l = x_r; }
+
+    double E_a = 1;
+
+    while ( E_a > 0.000001 ) {
+        double x_rnew = x_u - (function(x_u) * (x_u - x_l))/(function(x_u) - function(x_l)) ;
+
+        E_a = fabs( (x_rnew - x_r) / x_rnew ) ;
+
+        x_r = x_rnew ;
+
+        printf( "\t %d\t %f\t %f\t %f\t %f\t %f\n", no, x_l, x_u, x_r, function(x_r), E_a);
+        no++;
+
+        if ( function(x_r)*function(x_l) < 0) { x_u = x_r; }
+        else { x_l = x_r; }
+
+    }
 }
